80455be2d6ca6bb96c4b3715ba5c47239649af3a
wangle <wangle@agree.com.cn>
2026-05-02 10:14:29 +0800

    refactor: update rust source with new lib name and chinese comments

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,7 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+// 了解有关 Tauri 命令的更多信息，请访问 https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    format!("你好，{}！你已从 Rust 收到问候！", name)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -10,5 +10,5 @@ pub fn run() {
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .expect("运行 tauri 应用程序时出错");
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,6 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+// 在发布模式下防止在 Windows 上出现额外的控制台窗口，请勿删除！！
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    tauri_app_lib::run()
+    docker_manager_lib::run()
 }
